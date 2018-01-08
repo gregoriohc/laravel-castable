ee51c509634ab0fc876daad13e48f9f6ae751f0e
Gregorio Hernandez Caso <gregoriohc@gmail.com>
2018-01-08 20:15:46 +0100

    Add missing config casters

--- config/castable.php
+++ config/castable.php
@@ -14,6 +14,11 @@ return [
     'casters' => [
         'point' => \Gregoriohc\Castable\Casters\Point::class,
         'multipoint' => \Gregoriohc\Castable\Casters\MultiPoint::class,
+        'linestring' => \Gregoriohc\Castable\Casters\LineString::class,
+        'multilinestring' => \Gregoriohc\Castable\Casters\MultiLineString::class,
+        'polygon' => \Gregoriohc\Castable\Casters\Polygon::class,
+        'multipolygon' => \Gregoriohc\Castable\Casters\MultiPolygon::class,
+        'geometrycollection' => \Gregoriohc\Castable\Casters\GeometryCollection::class,
     ],
 
 ];
